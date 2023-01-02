4a7371c02640473502851543f78a8b15a197063b
Umair1181 <umairmaqbool.007@gmail.com>
2023-01-02 23:48:23 +0500

    Commit index

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 /////////////////// Coros Origin Configuraion /////////////////////////////
-const cors = require("cors");
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const cors = require("cors");
+// const corsOptions = {
+//   origin: "*",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 /////////////////// Data Parsers /////////////////////////////
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ parameterLimit: 100000, limit: "50mb" }));
@@ -22,12 +22,12 @@ mongoose
     console.log("db connected");
 
     ///////////////// API ROUTES ////////////////
-    app.use("/api", cors(corsOptions), require("./Routes/index"));
-    app.use("/image", cors(corsOptions), require("./storage/file"));
+    app.use("/api", require("./Routes/index"));
+    app.use("/image", require("./storage/file"));
     // app.use("/api", require("./Routes/attendence"));
     // app.use("/upload", require("./Api/UploadDocs"));
 
-    app.get("/", cors(corsOptions), (req, res) => {
+    app.get("/", (req, res) => {
       console.log("default root ");
       // return res.status(200).json({ msg: "server running..." });
       res.send("Server Runing");
