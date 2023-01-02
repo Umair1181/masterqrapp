03eb33c6b27f93a35a602fc965a33c2bf65a320a
Umair1181 <umairmaqbool.007@gmail.com>
2023-01-03 00:56:22 +0500

    Commit index

--- index.js
+++ index.js
@@ -13,33 +13,56 @@ app.use(express.json({ parameterLimit: 100000, limit: "50mb" }));
 
 ///////////////////DATABASE  CONFIGURATION  ///////////////////
 const db = require("./Config/db").mongodbOnline;
-mongoose.set("strictQuery", false);
-console.log("before db connection ");
-mongoose
-  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then((m) => {
-    global.mongodbconndbs = m.connection;
-    console.log("db connected");
+// mongoose.set("strictQuery", false);
+// console.log("before db connection ");
+// mongoose
+//   .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+//   .then((m) => {
+//     global.mongodbconndbs = m.connection;
+//     console.log("db connected");
 
-    ///////////////// API ROUTES ////////////////
-    app.use("/api", require("./Routes/index"));
-    app.use("/image", require("./storage/file"));
-    // app.use("/api", require("./Routes/attendence"));
-    // app.use("/upload", require("./Api/UploadDocs"));
+//     ///////////////// API ROUTES ////////////////
+//     app.use("/api", require("./Routes/index"));
+//     app.use("/image", require("./storage/file"));
+//     // app.use("/api", require("./Routes/attendence"));
+//     // app.use("/upload", require("./Api/UploadDocs"));
 
-    app.get("/", (req, res) => {
-      console.log("default root ");
-      // return res.status(200).json({ msg: "server running..." });
-      res.send("Server Runing");
-    });
-  })
-  .catch((err) => {
-    console.log("Db Connection Error");
-    console.log(err.response);
-  });
+//     app.get("/", (req, res) => {
+//       console.log("default root ");
+//       // return res.status(200).json({ msg: "server running..." });
+//       res.send("Server Runing");
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("Db Connection Error");
+//     console.log(err.response);
+//   });
 
 ///////////// PORT ENVOIRMENT //////////////////
-const port = process.env.PORT || 5000;
-const servera = app.listen(port, () => {
-  console.log(`SERVER RUNNING AT PORT ${port}`);
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(db);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+//Connect to the database before listening
+const PORT = process.env.PORT || 3000;
+connectDB().then((conn) => {
+  global.mongodbconndbs = conn.connection;
+  //Routes go here
+  app.use("/api", require("./Routes/index"));
+  // app.use("/image", require("./storage/file"));
+  app.all("*", (req, res) => {
+    res.json({ "every thing": "is awesome" });
+  });
+  app.listen(PORT, () => {
+    console.log(`listening for requests ${PORT}`);
+  });
 });
