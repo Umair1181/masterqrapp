353438b418db85d4eb92f038b20767047700d942
Umair1181 <umairmaqbool.007@gmail.com>
2023-01-09 03:23:38 +0500

    Commit Work

--- Routes/index.js
+++ Routes/index.js
@@ -4,7 +4,7 @@ Router.use("/student", require("../Api/Auth/student"));
 Router.use("/admin", require("../Api/Auth/admin"));
 Router.use("/teacher", require("../Api/Auth/teacher"));
 Router.use("/subject", require("../Api/Attendences/subject"));
-Router.use("/course", require("../Api/course"));
+Router.use("/course", require("../Api/Course"));
 
 Router.use("/attendence", require("../Api/Attendences/attendence"));
 Router.use("/enrolments", require("../Api/Enrolments"));
